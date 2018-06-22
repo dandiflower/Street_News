f68a5e38988f709882fde22fd8d51d9de74547f1
Jessica Brush <jessicadawnbrush@gmail.com>
2018-06-21 21:30:40 -0700

    ubu2

--- public/app.js
+++ public/app.js
@@ -3,13 +3,13 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append("<br><br><card data-id='" + data[i]._id + "'>" + data[i].title + "<br /><img src=" + data[i].link + " style='width: 150px'></card><br>");
   }
 });
 
 
 // Whenever someone clicks a p tag
-$(document).on("click", "p", function() {
+$(document).on("click", "card", function() {
   // Empty the notes from the note section
   $("#notes").empty();
   // Save the id from the p tag
